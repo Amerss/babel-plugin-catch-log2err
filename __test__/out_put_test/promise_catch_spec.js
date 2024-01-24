1ff9b92cc2ec194330893824494feea5716460a9
Amerss <13477725227@163.com>
2024-01-24 12:01:38 +0800

    保存修改

--- __test__/out_put_test/promise_catch_spec.js
+++ __test__/out_put_test/promise_catch_spec.js
@@ -1,12 +1,17 @@
 const getUerInfo = () => {
   return Promise.resolve('error');
 };
+
+// getUerInfo().then(res => {
+//     console.log(res)
+// }).catch(e => {
+//     flag = false
+//     console.log('123123123', e)
+//     console.log('123123123', e, '123123123')
+//     console.log(1 + 1)
+//     console.log(e)
+// })
+
 getUerInfo().then(res => {
   console.log(res);
-}).catch(e => {
-  flag = false;
-  console.error('123123123', e);
-  console.error('123123123', e, '123123123');
-  console.log(1 + 1);
-  console.error(e);
 });
--- __test__/promise_catch_spec.js
+++ __test__/promise_catch_spec.js
@@ -2,12 +2,16 @@ const getUerInfo = () => {
     return Promise.resolve('error')
 }
 
+// getUerInfo().then(res => {
+//     console.log(res)
+// }).catch(e => {
+//     flag = false
+//     console.log('123123123', e)
+//     console.log('123123123', e, '123123123')
+//     console.log(1 + 1)
+//     console.log(e)
+// })
+
 getUerInfo().then(res => {
     console.log(res)
-}).catch(e => {
-    flag = false
-    console.log('123123123', e)
-    console.log('123123123', e, '123123123')
-    console.log(1 + 1)
-    console.log(e)
 })
